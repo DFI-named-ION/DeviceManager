d43fc30234010d4b0246e5e90f7710c788d6a0e3
DFI-named-ION <larityandselnter@gmail.com>
2024-02-14 23:28:46 -0500

    Add project files.

---
+++ DeviceManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DeviceManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
